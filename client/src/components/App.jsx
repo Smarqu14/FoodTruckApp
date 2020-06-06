cfee27248a07e720bd1cd1eecfc5b7cebcad90d2
Steve Marquez <steveantonio00@gmail.com>
2020-06-06 13:21:38 -0700

    Install react-router

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Navigation from './Navigation';
+
 
 class App extends Component {
   render() {
     return (
       <div>
         Hello World
+        <Navigation />
       </div>
     );
   }
