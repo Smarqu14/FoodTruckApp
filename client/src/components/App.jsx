20dbf83b8db1d2d5476c90d3cfe2d7d3b4cd8b07
Steve Marquez <steveantonio00@gmail.com>
2020-06-06 20:40:40 -0700

    Implement image component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navigation from './nav/Navigation';
+import Slider from './ImageCarousel/Slider';
 
 
 const App = () => (
   <Router>
     <div className="App">
       <Navigation />
+      <Slider />
       <Switch>
         {/* <Route path="/" component={App} exact /> */}
         {/* <Route path="/home" component={home}/>
           <Route path="/hours" component={hours}/>
           <Route path="/booking" component={booking}/>
           <Route path="/order" component={order}/> */}
+
       </Switch>
     </div>
   </Router>
---
+++ client/src/components/ImageCarousel/Slider.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+export default class Slider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentIdx: 0,
+      images: [
+        'https://freetablephotos.s3-us-west-1.amazonaws.com/11.jpg',
+        'https://freetablephotos.s3-us-west-1.amazonaws.com/1.jpg',
+        'https://freetablephotos.s3-us-west-1.amazonaws.com/2.jpg',
+        'https://freetablephotos.s3-us-west-1.amazonaws.com/3.jpg',
+        'https://freetablephotos.s3-us-west-1.amazonaws.com/4.jpg',
+        'https://freetablephotos.s3-us-west-1.amazonaws.com/5.jpg',
+        'https://freetablephotos.s3-us-west-1.amazonaws.com/6.jpg',
+        'https://freetablephotos.s3-us-west-1.amazonaws.com/7.jpg',
+        'https://freetablephotos.s3-us-west-1.amazonaws.com/8.jpg',
+        'https://freetablephotos.s3-us-west-1.amazonaws.com/9.jpg',
+      ],
+      image: 'https://freetablephotos.s3-us-west-1.amazonaws.com/11.jpg',
+    };
+    this.prevImage = this.prevImage.bind(this);
+    this.nextImage = this.nextImage.bind(this);
+  }
+
+  prevImage() {
+    const { images, currentIdx } = this.state;
+    const lastIdx = images.length - 1;
+    const resetIdx = currentIdx === 0;
+    const idx = resetIdx ? lastIdx : currentIdx - 1;
+
+    this.setState({
+      currentIdx: idx,
+    });
+  }
+
+  nextImage() {
+    const { images, currentIdx } = this.state;
+    const lastIdx = images.length - 1;
+    const resetIdx = currentIdx === lastIdx;
+    const idx = resetIdx ? 0 : currentIdx + 1;
+
+    this.setState({
+      currentIdx: idx,
+    });
+  }
+
+  render() {
+    return (
+      <div className="slider">
+         <img src={this.state.images[5]}></img> 
+      </div>
+    );
+  }
+}
