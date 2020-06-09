39af76e89d1ae56e5dfb68609d7ac30b4c875b24
Steve Marquez <steveantonio00@gmail.com>
2020-06-09 04:19:39 -0700

    Render data from db

--- db/index.js
+++ db/index.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
+  user: 'stevemarquez',
+  host: 'localhost',
+  database: 'foodtruck',
   password: '',
   port: 5432,
 });
+
 module.exports = pool;
