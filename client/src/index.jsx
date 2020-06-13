3cc619c953a304edd62fbefe91aa633d393b226f
Steve Marquez <steveantonio00@gmail.com>
2020-06-13 00:04:38 -0700

    import browserRouter as Router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 import data from './data/dummyData';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App data={data} />
-  </BrowserRouter>, document.getElementById('app'),
+  </Router>,
+  document.getElementById('app'),
 );
