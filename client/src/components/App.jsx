46d1f371e9b08e7023f4a63157f9fb548cbc9ac4
Steve Marquez <steveantonio00@gmail.com>
2020-06-19 20:51:03 -0700

    Fix App component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Navigation from './nav/Navigation';
 import Routes from './nav/routes/routes';
+
+const FixedBackgroundImage = styled.div`
+  width: 100%;
+  height: 50px;
+  position: fixed;
+  top: 60px;
+  z-index: 2;
+  background-image: url(https://freetablephotos.s3-us-west-1.amazonaws.com/tomatos2.png);
+`;
 
 const App = ({ data }) => (
   <div>
     <Navigation />
+    <FixedBackgroundImage />
     <Routes data={data} />
   </div>
 );
